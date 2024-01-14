c2d1c1e4dd0eba346457e91864b9884f7823ed6f
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-01-14 15:11:31 +1100

    Create Ship class with class constructor method

--- src/battleShip.js
+++ src/battleShip.js
@@ -1,3 +1,18 @@
 const sum = (a, b) => a + b;
 
 export default sum;
+
+class Ship {
+  constructor(length, hitCounter = 0) {
+    this.length = length;
+    this.hitCounter = hitCounter;
+  }
+
+  hit() {
+    this.hitCounter += 1;
+  }
+
+  isSunk() {
+    return this.length === this.hitCounter ? true : false;
+  }
+}
